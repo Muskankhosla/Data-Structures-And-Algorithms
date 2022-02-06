50afa029acf9b8ddb76ca5770717ec8bc7e121ad
Muskan Khosla <khosla.muskan01@gmail.com>
2022-02-06 21:27:58 +0530

    Create 1. Selection Sort.cpp

---
+++ Sorting Algorithms/1. Selection Sort.cpp
@@ -0,0 +1,37 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    int temp;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+        
+    }
+    for(int i=0;i<n-1;i++)
+    { 
+        
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[j]<a[i])
+            {  
+                temp=a[j];
+                a[j]=a[i];
+                a[i]=temp;
+                
+            }
+        }
+    }
+     for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+        
+    }
+
+    return 0;
+}
